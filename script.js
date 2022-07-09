2926819957e9914aea9b59ef7e8813a59bc7d9c7
Thomas Aurelius <teg824@hotmail.com>
2022-07-09 00:07:59 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const ratingButtons = document.querySelectorAll('.rating-btn');
+const submitButton = document.querySelector('.submit-btn');
+const ratingCard = document.querySelector('.rating-card');
+const thankYouCard = document.querySelector('.thank-you-card');
+const ratingNumber = document.querySelector('.rating-number');
+
+const active = "active";
+let rating
+
+ratingButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const clickedRating = e.target
+        ratingButtons.forEach(button => {
+            button.classList.remove(active);
+        });
+        clickedRating.classList.add(active);
+         rating = clickedRating.innerText;
+         
+    });
+});
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    ratingCard.classList.add('hidden');
+    thankYouCard.classList.remove('hidden');
+    ratingNumber.innerText = rating;
+})
